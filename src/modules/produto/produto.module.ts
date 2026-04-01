48a236efeb648d6c24d00c94f75d9ba63b0e0ebe
Natália Fernandes <criativosnatalia@gmail.com>
2026-04-01 10:20:46 -0300

    removendo pastas

--- src/modules/produto/produto.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ProdutoController } from "./produto.controller";
-
-@Module({
-    imports:[],
-    controllers: [ProdutoController],
-    providers: []
-})
-export class ProdutoModule {}
